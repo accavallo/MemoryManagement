cdd23ac9b6e742d802621750a12f4869e19045c4
Tony <anthony.c.cavallo@gmail.com>
2016-11-28 18:19:49 -0600

    Initial Commit

---
+++ Proj6/Proj6.h
@@ -0,0 +1,33 @@
+//
+//  Proj6.h
+//  Proj6
+//
+//  Created by Tony on 11/28/16.
+//  Copyright © 2016 Anthony Cavallo. All rights reserved.
+//
+
+#ifndef Proj6_h
+#define Proj6_h
+
+#include <stdio.h>          /* Basic functions */
+//#include <stdlib.h>         /* Some other basic functions such as exit and EXIT_FAILURE */
+//#include <unistd.h>         /* For execl */
+//#include <signal.h>         /* For signal handling */
+//#include <getopt.h>         /* For command line options */
+//#include <string.h>         /* atoi(string) */
+//#include <ctype.h>          /* For isprint in getopt */
+#include <time.h>           /* srand(unsigned) */
+//#include <sys/time.h>       /* POSIX standard, same as time.h */
+//#include <sys/shm.h>        /* For shared memory */
+//#include <sys/msg.h>        /* For message passing */
+//#include <semaphore.h>      /* For semaphore usage */
+//#include <stdbool.h>        /* For bool type because C doesn't include it automatically. I really hate this language */
+//#include <sys/ipc.h>        /* For inter-process communication */
+//#include <sys/types.h>      /*  */
+//#include <errno.h>          /* To set errno for perror */
+//#include <fcntl.h>          /* For O_* constants */
+//#include <sys/stat.h>       /* For mode constants */
+
+/* Function prototypes for OSS */
+
+#endif /* Proj6_h */
---
+++ Proj6/oss.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Proj6
+//
+//  Created by Tony on 11/28/16.
+//  Copyright © 2016 Anthony Cavallo. All rights reserved.
+//
+
+#include "Proj6.h"
+
+int main(int argc, const char * argv[]) {
+    printf("Hello from oss, World!\n");
+    return 0;
+}
