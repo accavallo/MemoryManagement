36a13d76f74d084a1321b2208011b09123105e14
Tony <anthony.c.cavallo@gmail.com>
2016-11-29 14:54:46 -0600

    Created the "page", set up the frame, created the processes that have page requests unique within each process, and tested out the circular link for the pages. Now I need to do page requests, faults and figure out what the optimal would have been.

--- Proj6/Proj6.h
+++ Proj6/Proj6.h
@@ -10,7 +10,7 @@
 #define Proj6_h
 
 #include <stdio.h>          /* Basic functions */
-//#include <stdlib.h>         /* Some other basic functions such as exit and EXIT_FAILURE */
+#include <stdlib.h>         /* Some other basic functions such as exit and EXIT_FAILURE and srand() */
 //#include <unistd.h>         /* For execl */
 //#include <signal.h>         /* For signal handling */
 //#include <getopt.h>         /* For command line options */
@@ -21,13 +21,27 @@
 //#include <sys/shm.h>        /* For shared memory */
 //#include <sys/msg.h>        /* For message passing */
 //#include <semaphore.h>      /* For semaphore usage */
-//#include <stdbool.h>        /* For bool type because C doesn't include it automatically. I really hate this language */
+#include <stdbool.h>        /* For bool type because C doesn't include it automatically. I really hate this language */
 //#include <sys/ipc.h>        /* For inter-process communication */
 //#include <sys/types.h>      /*  */
 //#include <errno.h>          /* To set errno for perror */
 //#include <fcntl.h>          /* For O_* constants */
 //#include <sys/stat.h>       /* For mode constants */
 
+/* Variables for OSS */
+typedef struct page_memory {
+    int use;
+    int pageNum;
+    struct page_memory *nextPage;
+}pm_t;
+pm_t *head;
+int currentPointer;
+enum {used = 1, notUsed = 0};
+
 /* Function prototypes for OSS */
+int **createProcessesWithPages();
+void printArray(int **);
+bool pageAlreadyRequested(int, int, int *);
+pm_t *createMemoryFrame();
 
 #endif /* Proj6_h */
