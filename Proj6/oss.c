2f58967115287823e965f63e0979a49071a2206f
Tony <anthony.c.cavallo@gmail.com>
2016-11-29 21:44:44 -0600

    Created the clock page replacement algorithm successfully and implemented getopt with some options. Still need to figure out the optimal page replacement algorithm and, if there's time, implement the Least Recently Used and First In, First Out algorithms. I don't need to do either, but I would like to have more options for testing purposes.

--- Proj6/oss.c
+++ Proj6/oss.c
@@ -8,21 +8,124 @@
 int main(int argc, const char * argv[]) {
     srand((unsigned)time(NULL));
     /* Create the processes with unique page requests within each process. */
-    int **processArray = createProcessesWithPages(), i;
-//    printArray(processArray);
     
-    head = createMemoryFrame();
-//    pm_t *current = head;
-//    for (i = 0; i < 10; i++) {
-//        printf("%i ", current->pageNum);
-//        current = current->nextPage;
-//    }
+    int **processArray = createProcessesWithPages(), i, j, option;
+    printArray(processArray);
     
-    /* Free the memory allocated for the processes */
-    for (i = 0; i < 10; i++)
-        free(processArray[i]);
-    free(processArray);
+    while ((option = getopt(argc, (char **)argv, "h")) != -1) {
+        switch (option) {
+            case 'h':
+                printf("Display help\n");
+                break;
+            case 'c':
+                printf("Page replacement algorithm set to clock.\n");
+                break;
+            case 'f':
+                printf("Page replacement algorithm set to first in, first out.\n");
+                break;
+            case 'l':
+                printf("Page replacement algorithm set to least recently used.\n");
+                break;
+            default:
+                break;
+        }
+    }
+    for (i = optind; i < argc; i++) {
+        printf("Non-option argument \"%s\"\n", argv[i]);
+    }
+    
+    head = createClockMemoryFrame();
+    pm_t *current = head;
+    printf("Pages in current frame:\n");
+    for (i = 0; i < totalPages; i++) {
+        printf("%i ", current->pageNum);
+        current = current->nextPage;
+    }
+    printf("\n");
+    
+    int faultCount = 0, successCount = 0;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 8; j++) {
+            if (faultOccurred(processArray[i][j], current)) {
+                current = replaceClockPage(current, processArray[i][j]);
+                faultCount++;
+            } else {
+                successCount++;
+            }
+        }
+    }
+    
+    printf("Faults:  %2i\n", faultCount);
+    printf("Success: %2i\n", successCount);
+    
+    printf("Pages in current frame after:\n");
+    for (i = 0; i < totalPages; i++) {
+        printf("%i ", current->pageNum);
+        current = current->nextPage;
+    }
+    printf("\n");
+    
+    getOptimalSolution(processArray);
+    
+    freeMemory(processArray);
     return 0;
+}
+
+/* Method to create the initial memory frame with no pages loaded. */
+pm_t *createClockMemoryFrame() {
+    pm_t *frame = (pm_t *)malloc(sizeof(pm_t)), *current;
+    frame->use = notUsed;
+    frame->nextPage = NULL;
+    frame->pageNum = 0;
+    current = frame;
+    int i;
+    for (i = 0; i < totalPages - 1; i++) {
+        pm_t *temp = (pm_t *)malloc(sizeof(pm_t));
+        //        bool pageExists;
+        //        int pageNum;
+        //        do {
+        //            pageNum = 1 + rand() % 50;
+        //        } while (pageExists);
+        temp->pageNum = 0;
+        
+        current->nextPage = temp;
+        current = current->nextPage;
+    }
+    current->nextPage = frame;
+    return frame;
+}
+
+/* Create a 2D array of "processes" that has "page requests". */
+int **createProcessesWithPages() {
+    int **array, i, j;
+    
+    array = (int **)malloc(sizeof(int *) * 10);
+    /******NOTE******
+     * These two numbers MUST remain at 10 and 8 respectively.
+     ****************/
+    for (i = 0; i < 10; i++) {
+        array[i] = (int *)malloc(sizeof(int) * 8);
+        for (j = 0; j < 8; j++) {
+            int page;
+            do {
+                page = 1 + rand() % 50;
+            } while (pageAlreadyRequested(page, j, array[i]));
+            array[i][j] = page;
+        }
+    }
+    return array;
+}
+
+/* Each process can only have 1 of each page request, so this method ensures that there are no duplicate requests within that process. */
+bool pageAlreadyRequested(int page, int index, int* process) {
+    if (index == 0)
+        return false;
+    int i;
+    for (i = 0; i < index; i++)
+        if (process[i] == page)
+            return true;
+    
+    return false;
 }
 
 /* Method to print the processArray when I need it to */
@@ -37,50 +140,61 @@ void printArray(int **array) {
     }
 }
 
-/* Create a 2D array of "processes" that has "page requests" */
-int **createProcessesWithPages() {
-    int **array, i, j;
-    
-    array = (int **)malloc(sizeof(int *) * 10);
-    
-    for (i = 0; i < 10; i++) {
-        array[i] = (int *)malloc(sizeof(int) * 8);
-        for (j = 0; j < 8; j++) {
-            int page;
-            do {
-                page = 1 + rand() % 50;
-            } while (pageAlreadyRequested(page, j, array[i]));
-            array[i][j] = page;
+/* Determine whether a page fault has occurred based on the page the current process is requesting and what is currently in memory. */
+bool faultOccurred(int page, pm_t *frame) {
+//    int count = 0;
+    pm_t *start = frame;
+    while (1) {
+        if (page == frame->pageNum)
+            return false;
+//        else
+//            frame->use = notUsed;
+        
+        frame = frame->nextPage;
+        if (frame == start) {
+            break;
         }
     }
-    return array;
-}
-
-/* Each process can only have 1 of each page request, so this method ensures that there are no duplicate requests within that process */
-bool pageAlreadyRequested(int page, int index, int* process) {
-    if (index == 0)
-        return false;
-    int i;
-    for (i = 0; i < index; i++)
-        if (process[i] == page)
-            return true;
     
-    return false;
+//    frame = frame->nextPage;
+    return true;
 }
 
-pm_t *createMemoryFrame() {
-    pm_t *frame = (pm_t *)malloc(sizeof(pm_t)), *current;
-    frame->use = notUsed;
-    frame->nextPage = NULL;
-    frame->pageNum = 69;
-    current = frame;
-    int i;
-    for (i = 0; i < 4; i++) {
-        pm_t *temp = (pm_t *)malloc(sizeof(pm_t));
-        temp->pageNum = frame->pageNum + i + 1;
-        current->nextPage = temp;
-        current = current->nextPage;
+/* Page fault occurred, now a page needs to be replaced within the frame. */
+pm_t *replaceClockPage(pm_t *frame, int pageNeeded) {
+    /* Go through the frame until we find a page that hasn't been used in a bit. */
+    while (frame->use != notUsed) {
+        frame->use = notUsed;
+        frame = frame->nextPage;
     }
-    current->nextPage = frame;
+    /* Replace the current page with the page that is needed, set the use bit to 1 and advance to the next page in the frame */
+    frame->pageNum = pageNeeded;
+    frame->use = used;
+    frame = frame->nextPage;
+    
     return frame;
+}
+
+void getOptimalSolution(int **processes) {
+
+}
+
+void freeMemory(int **array) {
+    int i;
+    /* Free the memory allocated for the processes */
+    for (i = 0; i < 10; i++)
+        free(array[i]);
+    free(array);
+    /* Free the memory allocated for the frame */
+    pm_t *deleteNode = head;
+    i = 0;
+    while (i < totalPages) {
+//        printf("deleteNode->pageNum: %i\n", deleteNode->pageNum);
+        pm_t *pointer = deleteNode->nextPage;
+        free(deleteNode);
+        deleteNode = pointer;
+        i++;
+    }
+    head = NULL;
+    deleteNode = NULL;
 }
